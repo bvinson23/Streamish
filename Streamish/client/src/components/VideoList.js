5d082e4546b0e9e42f15885967f4154ac476c9bd
bvinson23 <brandonvinson23@gmail.com>
2021-07-09 12:04:44 -0500

    user can search videos by title

--- Streamish/client/src/components/VideoList.js
+++ Streamish/client/src/components/VideoList.js
@@ -1,27 +1,56 @@
 import React, { useEffect, useState } from "react";
 import Video from './Video';
-import { getAllVideos } from "../modules/videoManager";
+import { getAllVideos, searchVideos } from "../modules/videoManager";
 
 const VideoList = () => {
     const [videos, setVideos] = useState([]);
+    const [search, setSearch] = useState("");
 
     const getVideos = () => {
         getAllVideos().then(videos => setVideos(videos));
     };
 
+    const handleSearch = (evt) => {
+        evt.preventDefault()
+        let searchInput = evt.target.value
+
+        if (searchInput.length > 0) {
+            let searchMatch = videos.filter(videos => {
+                if (videos.title.toLowerCase().includes(searchInput.toLowerCase())) {
+                    return true
+                }
+            })
+            setVideos(searchMatch)
+        }
+        else {
+            getVideos()
+        }
+    }
+
+
     useEffect(() => {
         getVideos();
     }, []);
 
+    useEffect(() => {
+        searchVideos();
+    }, [search]);
+
     return (
-        <div className="container">
-            <div className="row justify-content-center">
-                {videos.map((video) => (
-                    <Video video={video} key={video.id} />
-                ))}
+        <>
+            <div>
+                <input type='text' className="search" required onChange={handleSearch} id="search_box" placeholder="Search" />
             </div>
-        </div>
+            <div className="container">
+                <div className="row justify-content-center">
+                    {videos.map((video) => (
+                        <Video video={video} key={video.id} />
+                    ))}
+                </div>
+            </div>
+        </>
     );
-};
+}
+
 
 export default VideoList;
