11193ce74ecfa44ddb9be546ba6a7d54b9a26a63
bvinson23 <brandonvinson23@gmail.com>
2021-07-09 09:29:41 -0500

    created video manager module and video list component

---
+++ Streamish/client/src/modules/videoManager.js
@@ -0,0 +1,18 @@
+const baseUrl = '/api/video';
+// This is a relative url -- it doesn't look anything like https://localhost:5001/api/video.
+// This is the benefit of adding the proxy attribute in our package.json file.
+
+export const getAllVideos = () => {
+  return fetch(baseUrl)
+    .then((res) => res.json())
+};
+
+export const addVideo = (video) => {
+  return fetch(baseUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(video),
+  });
+};
